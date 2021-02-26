8d24a598b97fb42e79cce034d7abee7453fb5d6f
Rachelle <rachellerathbone@gmail.com>
2021-02-26 14:00:50 +1100

    PLAYG-1 my god!!!

--- easy/removeVowels/test.js
+++ easy/removeVowels/test.js
@@ -4,8 +4,6 @@ test('removeVowels is a function', () => {
   expect(typeof removeVowels).toEqual('function');
 });
 
-
-
 test('removeVowels returns the element that occurs the most in an array', () => {
   expect(removeVowels("leetcodeisacommunityforcoders")).toEqual("ltcdscmmntyfrcdrs");
   expect(removeVowels("aeiou")).toEqual("");
